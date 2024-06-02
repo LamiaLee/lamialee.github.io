6331755b22b2041c041a24fff67fdb06ed6195b1
ArthurAns <arthuransquer@gmail.com>
2024-06-02 15:26:54 +0200

    Updates

--- src/components/SizzleCaseStudy/Ellipse40Icon.tsx
+++
@@ -1,9 +0,0 @@
-import { memo, SVGProps } from 'react';
-
-const Ellipse40Icon = (props: SVGProps<SVGSVGElement>) => (
-  <svg preserveAspectRatio='none' viewBox='0 0 21 21' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-    <circle cx={10.5} cy={10.5} r={10.5} fill='#E32227' fillOpacity={0.2} />
-  </svg>
-);
-const Memo = memo(Ellipse40Icon);
-export { Memo as Ellipse40Icon };
